316f0a441cf33759edda0b82545c4f7b9a99d377
damiandoles <damiandoles@gmail.com>
2017-07-27 14:44:28 +0200

    Obsługa daty i godziny; Clean up

--- website/source/scripts/scripts.js
+++ website/source/scripts/scripts.js
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 
+var IndTID;
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 function TempControlMode()
 {
@@ -92,4 +93,84 @@ function SaveIrrigationSettings()
 function CalibrateProbe()
 {
     alert("Calibration started!");
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+function CheckIP(AIP, AAllow00, AAllowFF)
+{
+    if (AIP && AIP !== "")
+    {
+        var Pattern = /^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/;
+        var A = AIP.match(Pattern);
+        if (A && (A.length == 5) )
+        {
+            for (i = 1; i < 5; i++)	
+                if ( (A[i] < 0) || (A[i] > 255) )
+                    return false;
+
+            if (!AAllow00)
+                if (A[1] == 0 && A[2] == 0 && A[3] == 0 && A[4] == 0)
+                    return false;
+
+            if (!AAllowFF)
+                if (A[1] == 255 && A[2] == 255 && A[3] == 255 && A[4] == 255)
+                    return false;
+
+            return true;
+        }
+    }
+    else 
+    {
+        return false;
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+function CheckShortTime(ATime)
+{
+    if (ATime && ATime !== "")
+    {
+        var Pattern = /^(\d{1,2})\:(\d{1,2})$/;
+        var A = ATime.match(Pattern);
+        if ((A) && (A.length == 3) && (A[1] < 24) && (A[2] < 60) )
+            return true;
+        else 
+            return false;
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+function SetIndexTimers()
+{
+    IndTID = setInterval('updateClock()', 1000 );
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+function OnIndexLoad()
+{
+    updateClock();
+    IndTID = setInterval('updateClock()', 1000 );
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+function updateClock()
+{
+    var dateTimeObj = new Date();
+    
+    var hoursStr = dateTimeObj.getHours();
+    if (hoursStr < 10) hoursStr = "0" + hoursStr;
+    
+    var minutesStr = dateTimeObj.getMinutes();
+    if (minutesStr < 10) minutesStr = "0" + minutesStr;
+    
+    var secondsStr = dateTimeObj.getSeconds();
+    if (secondsStr < 10) secondsStr = "0" + secondsStr;
+    
+    var timeString = hoursStr + ":" + minutesStr + ":" + secondsStr;
+
+    var monthDayStr = dateTimeObj.getDate();
+    if (monthDayStr < 10) monthDayStr = "0" + monthDayStr;
+    
+    var monthStr = (dateTimeObj.getMonth() + 1);
+    if (monthStr < 10) monthStr = "0" + monthStr;
+       
+    var dateString = monthDayStr + "/" + monthStr + "/" + dateTimeObj.getFullYear();
+ 
+    document.getElementById("T_01").value = timeString;
+    document.getElementById("T_02").value = dateString;
 }
